93bc218de5a0d5eeb3a31054c6f91b102bd5bb88
Looper <aayush.bhattacharjee2002@gmail.com>
2024-02-19 17:44:53 +0530

    added movement feature with win keys

--- main.py
+++ main.py
@@ -1,12 +1,42 @@
 import tkinter as tk
-from PIL import ImageTk, Image  
+from PIL import ImageTk, Image
+
+
+def left(e):
+    x = -20
+    y = 0
+    canvas.move(img, x, y)
+
+
+def right(e):
+    x = 20
+    y = 0
+    canvas.move(img, x, y)
+
+
+def up(e):
+    x = 0
+    y = -20
+    canvas.move(img, x, y)
+
+
+def down(e):
+    x = 0
+    y = 20
+    canvas.move(img, x, y)
+
 
 r = tk.Tk()
 r.geometry("800x800")
-r.title('Car MainLoop')
-car_main = Image.open("./assets/car_top_view.png")
-tkimage= ImageTk.PhotoImage(car_main)
-label=tk.Label(r,image=tkimage)
-label.place(relx=0.0, rely=10.0, anchor='sw')
-label.pack(side=tk.BOTTOM)
+r.title("Car MainLoop")
+canvas = tk.Canvas(r, width=800, height=800, bg="white")
+canvas.pack(pady=20)
+image = ImageTk.PhotoImage(Image.open("./assets/car_top_view.png"))
+img = canvas.create_image(250, 120, anchor="nw", image=image)
+
+r.bind("<Left>", left)
+r.bind("<Right>", right)
+r.bind("<Up>", up)
+r.bind("<Down>", down)
+
 r.mainloop()
